d483b3f1847e895eb097c4989f917436696e390a
Falthera <223241157+Falthera@users.noreply.github.com>
2026-08-15 12:46:44 +1000

    fix: add windows/darwin build tags to BLE scanners

--- internal/discovery/ble/winrt.go
+++ internal/discovery/ble/winrt.go
@@ -1,3 +1,5 @@
+//go:build windows
+
 // ReverseDrop - Copyright (C) ReverseDrop Contributors
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
